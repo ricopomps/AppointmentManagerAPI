fb79f967b46d05f91abc34a0333811ab159206ba
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-06-14 14:19:19 -0300

    Feat: Implementing cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import morgan from "morgan";
+import cors from "cors";
 import express, { NextFunction, Response, Request } from "express";
 import session from "express-session";
 import MongoStore from "connect-mongo";
@@ -14,6 +15,7 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
+app.use(cors());
 
 app.use(
   session({
